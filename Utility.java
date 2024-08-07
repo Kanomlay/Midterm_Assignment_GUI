3e60a45aeb602ec53e4fc0be5378c31055be064c
MiGKP <103379171+MiGKP@users.noreply.github.com>
2024-08-08 02:17:53 +0700

    Add files via upload

--- Utility.java
+++ Utility.java
@@ -2,26 +2,25 @@ import java.awt.Color;
 
 public class Utility {
 
-    public static int getHealthyPopulation(int pm25) {
-        double sickPercentage = 0.0;
+    public static Color getColorForHealthRisk(int pm25) {
+        double percentageSick;
         if (pm25 >= 0 && pm25 <= 50) {
-            sickPercentage = Math.random() * 0.09; // 0-9%
+            percentageSick = Math.random() * 0.09; // 0-9%
         } else if (pm25 >= 51 && pm25 <= 100) {
-            sickPercentage = 0.10 + Math.random() * 0.09; // 10-19%
+            percentageSick = 0.10 + Math.random() * 0.09; // 10-19%
         } else if (pm25 >= 101 && pm25 <= 150) {
-            sickPercentage = 0.20 + Math.random() * 0.09; // 20-29%
+            percentageSick = 0.20 + Math.random() * 0.09; // 20-29%
         } else if (pm25 >= 151 && pm25 <= 250) {
-            sickPercentage = 0.30 + Math.random() * 0.20; // 30-50%
+            percentageSick = 0.30 + Math.random() * 0.20; // 30-50%
+        } else {
+            percentageSick = 0.0; // ตั้งให้ค่าเป็น 0 ถ้า pm25 น้อยกว่า 0 หรือมากกว่า 250
         }
-        return (int) (sickPercentage * 100);
-    }
-    
-    public static Color getColorForHealthRisk(int sickPercentage) {
-        if (sickPercentage >= 30) {
+
+        if (percentageSick > 0.30) {
             return Color.RED; // 30% or more
-        } else if (sickPercentage >= 20) {
+        } else if (percentageSick > 0.20) {
             return Color.ORANGE; // 20-29%
-        } else if (sickPercentage >= 10) {
+        } else if (percentageSick > 0.10) {
             return Color.YELLOW; // 10-19%
         } else {
             return Color.GREEN; // 0-9%
