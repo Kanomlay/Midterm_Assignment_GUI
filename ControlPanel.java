8a4a53a687fd447856ecdf72e045dd382e49d784
Kanomlay <165756752+Kanomlay@users.noreply.github.com>
2024-08-10 01:45:23 +0700

    📝
    เพิ่มเมื่อกดปุ่มฝนเทียมจะเปลี่ยนสีเพื่อบอกว่ามันกำลังทำงาน

--- ControlPanel.java
+++ ControlPanel.java
@@ -9,13 +9,16 @@ public class ControlPanel extends JPanel {
     private JButton random_rain;
     private JButton back;
     private JButton add_files;
+    private JButton add_population;
     private CalculateProcess cal;
     private DataPanel data;
-
+   /*private JTextField textField_1;
+    private JTextField textField_2;*/
     public ControlPanel(CalculateProcess cal, int[][] pm25, JButton[][] buttons, int[][] populations) {
 
         this.cal = cal;
-        setLayout(new GridLayout(1, 4, 10, 10));
+        
+        setLayout(new GridLayout(1, 5, 10, 10));
         setBackground(new Color(174, 214, 241));
 
         back = new JButton("BACK");
@@ -34,10 +37,14 @@ public class ControlPanel extends JPanel {
                 cal.toggleArtificialRainMode();
                 if (cal.isArtificialRainMode()) {
                     button_rain.setText("Exit Mode Artificial");
+                    button_rain.setBackground(new Color(169, 50, 38));
+                    button_rain.setForeground(Color.WHITE);
                     cal.useFonTaerm();
                 }
                 else{
                     button_rain.setText("Artificial Rain");
+                    button_rain.setBackground(new Color(133, 193, 233));
+                    button_rain.setForeground(Color.BLACK);
                     cal.updateButtons();
                 }
             }
@@ -64,12 +71,28 @@ public class ControlPanel extends JPanel {
                 cal.loadFile();
             }
         });
+        add_population = new JButton("Add Population");
+        add_population.setPreferredSize(new Dimension(100 , 50));
+        add_population.setBackground(new Color(133, 193, 233));
+        add_population.setForeground(Color.BLACK);
+
+
 
         add(back);
         add(button_rain);
         add(random_rain);
         add(add_files);
+        add(add_population);
+
+        /*textField_1 = new JTextField();
+        textField_1.setBounds(700, 10, 100, 25);
+        textField_2 = new JTextField();
+        textField_2.setBounds(810, 10, 100, 25);
+
+        add(textField_1);
+        add(textField_2);*/
     }
+
 
 
     public JButton getBack() {
@@ -87,6 +110,10 @@ public class ControlPanel extends JPanel {
     public JButton getAddFiles() {
         return add_files;
     }
+    
+    public JButton getAddPopulation() {
+        return add_population;
+    }
     /*public void setBackActionListener(CardLayout cardLayout,JPanel maiPanel){
        getBack().addActionListener(new ActionListener() {
 
@@ -98,4 +125,10 @@ public class ControlPanel extends JPanel {
             
         });
     }*/
+    
+/*=====================================================================================*/
+
+    
+    
 }
+
